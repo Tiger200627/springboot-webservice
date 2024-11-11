e97fab5ae47fbe23fb15e033dff7cdfafe15be44
hyojin <tiger275458@gmail.com>
2024-11-12 02:27:38 +0900

    JPA 다뤄보기2 - H2 의존성 추가 - 코드 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("com.h2database:h2") // 인메모리 관계형 데이터베이스(별도 설치 X, JPA 테스트용, 재시작 시 초기화됨)
 }
 
 tasks.withType<Test> {
--- src/main/java/com/example/springboot_webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/example/springboot_webservice/domain/posts/PostsRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 //@Repository
-//@Repository
 public interface PostsRepository extends JpaRepository<Posts, Long>{
 }
